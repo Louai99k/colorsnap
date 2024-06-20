5bc36d4b4c268d9eb0e2a9bd419606194cbb6cbc
Louai99k <louaikelani99k@gmail.com>
2024-06-20 18:16:56 +0300

    update the script to make it more user friendly

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Create a release directory and enter it
 mkdir -p release
 cd release
+
+# Configure the project with cmake as a release version
 cmake -DCMAKE_BUILD_TYPE=Release ../
-sudo make install
+
+# Build the project
+make
+
+# Return back to original directory
+cd -
